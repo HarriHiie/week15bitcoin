8191a0b620d718d12925cbd2169db06f1d4fc541
Opilane TTHK <opilane@tthk.ee>
2021-12-07 16:04:15 +0200

    bitcoin

---
+++ BitcoinRate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitcoinRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
